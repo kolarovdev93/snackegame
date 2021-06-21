9f81ec7a8395e2b0a1f5e62387c925f4b2b7cc24
kolarovdev93 <devkolarov27@gmail.com>
2021-06-21 19:50:17 +0100

    adicionamos a cobrinha na box

--- script.js
+++ script.js
@@ -4,11 +4,38 @@ let context = canvas.getContext("2d");
 
 let box = 32;
 
+let snack = [];
+
+snack[0]={
+    x: 8 * box,
+    y: 8 * box
+}
+
 function criarBG(){//criar fundo
     context.fillStyle = "lightgreen";//dar um contraste com o estilo do canvas
 
     context.fillRect(0, 0, 16 * box, 16 * box);//desenhar o rectangulos com aquelas coordenadas
 }
 
+/*a cobrinha é uma array
+
+as coordenadas serão x e y.
+
+a cobra é um array que a medida que esta
+anda será incrementado ou decrementado
+
+*/
+function criarCobrihna(){
+    for(i = 0; i < snack.length; i++){
+        context.fillStyle = "green";
+
+        context.fillRect(snack[i].x, snack[i].y, box, box);
+        /**
+         * 
+         * Aqui colocamos a cobra na box
+         */
+    }
+}
 
 criarBG();
+criarCobrihna();
