a192038b863163e9cb3d7dd666c2db0300cb1bdc
kolarovdev93 <devkolarov27@gmail.com>
2021-06-21 20:11:16 +0100

    definir movimento da cobra

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ let box = 32;
 
 let snack = [];
 
+/**
+ * Coordenadas x e y
+ * para colocar a cobra inicialmente 
+ * numa posição definida a baixo
+ */
 snack[0]={
     x: 8 * box,
     y: 8 * box
 }
+
+let direction = "right";
 
 function criarBG(){//criar fundo
     context.fillStyle = "lightgreen";//dar um contraste com o estilo do canvas
@@ -37,5 +44,49 @@ function criarCobrihna(){
     }
 }
 
-criarBG();
-criarCobrihna();
+/**
+ * Criar uma função que faça que a cobra ande
+ * de tempo em tempo
+ */
+
+function inicialJogo(){
+    /**
+     * Chamar as primeiras funcoes
+     */
+     criarBG();
+     criarCobrihna();
+
+     /**
+      * Definir a posição inicial da cobrina
+      */
+
+     let snakeX = snack[0].x;
+     let snakeY = snack[0].y;
+
+    //Criar as coordenadas da cobrinha
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snack.pop();
+
+    let newHead = {
+        x: snackX,
+        y: snakeY
+    }
+
+    snack.unshift(newHead);
+}
+/**
+ * Começar o jogo no intervalo em 100 milisegundo
+ * depois é renovada assim que ele travar
+ */
+let jogo = setInterval(inicialJogo, 100);
+
+
+
+
+
+
