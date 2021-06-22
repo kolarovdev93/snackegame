a409a0a5351c20663987bdcf78831756e57efa61
kolarovdev93 <devkolarov27@gmail.com>
2021-06-22 12:54:48 +0100

    Criar Cobrinha e adicionar ao Canvas

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let canvas = document.getElementById('snack');
 let context = canvas.getContext("2d");
 let box = 32;
+
+/**
+ * A COBRA PRECISA 
+ * DE SER UM ARRAY
+ */
+
+
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
  /**
   * CRIAR UMA FUNÇÃO QUE VAI
   * CRIAR O CANVAS
@@ -10,4 +24,13 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+
+function criarCobrinha(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
