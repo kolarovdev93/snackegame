2798004f52a6cc3fab9f087796905d30e92baecf
kolarovdev93 <devkolarov27@gmail.com>
2021-06-22 13:54:02 +0100

    Adicionar comidinha de forma aleatoria

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ snake[0] = {
 
 let direction = "right";
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
  /**
   * CRIAR UMA FUNÇÃO QUE VAI
@@ -34,6 +38,12 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 
@@ -56,10 +66,11 @@ function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].x = 0;
-    if(snake[0].y < 0 * box && direction == "up") snake[0].x = 16 * box;
+    if(snake[0].y < 0 && direction == "up") snake[0].x = 16 * box;
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
